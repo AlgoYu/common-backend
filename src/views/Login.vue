1fe665951cbb86e6ad38c1d42ad17f3b3341f9bd
AnyDev <794763733@qq.com>
2020-06-18 20:07:42 +0800

    add face detect login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,14 @@
 <template>
 	<div class="container">
 		<el-card class="box-card">
-			<section class="login-panel">
+			<section class="option-panel">
+				<el-button circle @click="changePanel">
+					<svg class="icon" aria-hidden="true" style="font-size: 25px;color: #909399;">
+						<use xlink:href="#icon-camera-fill"></use>
+					</svg>
+				</el-button>
+			</section>
+			<section v-if="!faceView" class="login-panel">
 				<img src="https://wx4.sinaimg.cn/large/0065B4vHgy1g7u65sx5jsj309s08jwek.jpg" style="width: 50px;" class="logo" />
 				<h2>后台管理系统</h2>
 				<el-form :model="form" :rules="rules" ref="loginForm" label-width="100px">
@@ -22,6 +29,10 @@
 					</div>
 				</el-form>
 			</section>
+			<section v-if="faceView" class="face-panel">
+				<h2 style="margin: 0 auto;">人脸识别</h2>
+				<video autoplay="autoplay" class="video-style"></video>
+			</section>
 		</el-card>
 	</div>
 </template>
@@ -29,14 +40,16 @@
 <script>
 	import md5 from 'js-md5';
 	import SwipeVerification from '../components/SwipeVerification.vue'
-	
+
 	export default {
 		name: 'Login',
-		components:{
+		components: {
 			SwipeVerification: SwipeVerification
 		},
 		data() {
 			return {
+				faceView: false,
+				interval: null,
 				form: {
 					accountName: '',
 					accountPassword: '',
@@ -71,9 +84,55 @@
 				confirmSuccess: false
 			}
 		},
+		watch: {
+			faceView(value) {
+				if (value) {
+					this.initCamera();
+				}else{
+					window.clearInterval(this.interval);
+				}
+			}
+		},
 		methods: {
+			changePanel() {
+				this.faceView = !this.faceView;
+			},
+			initCamera() {
+				let self = this;
+				navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+				if (navigator.getUserMedia) {
+					//调用用户媒体设备, 访问摄像头
+					navigator.getUserMedia({
+						video: {
+							width: 800,
+							height: 400
+						}
+					}, function(stream) {
+						var video = document.querySelector('video');
+						video.srcObject = stream;
+						self.streamPicture = stream; //关闭摄像头需要用
+						video.onloadedmetadata = function(e) {
+							video.play();
+						};
+						video.addEventListener('loadeddata', function(){
+							var canvas = document.createElement("canvas");
+							canvas.width = video.videoWidth;
+							canvas.height = video.videoHeight;
+							self.interval = window.setInterval(function(){
+								canvas.getContext('2d').drawImage(video, 0, 0, canvas.width,canvas.height);
+								let url = canvas.toDataURL("image/png");
+								console.log(url);
+							},3000);
+						});
+					}, function(err) {
+						console.log("访问用户媒体设备失败: " + err.name);
+					})
+				} else {
+					self.$message.error('不支持访问用户媒体');
+				}
+			},
 			login(formName) {
-				if(!this.confirmSuccess){
+				if (!this.confirmSuccess) {
 					this.$message.error('请完成滑动验证！');
 					return;
 				}
@@ -115,7 +174,7 @@
 					}
 				});
 			},
-			confirmSuccessChanged(value){
+			confirmSuccessChanged(value) {
 				this.confirmSuccess = value;
 			},
 			resetForm(formName) {
@@ -134,16 +193,37 @@
 		align-items: center;
 	}
 
+	.option-panel {
+		display: flex;
+		flex-direction: row-reverse;
+	}
+
 	.login-panel {
-		width: 600px;
+		width: 500px;
 		height: 400px;
+		padding-top: 20px;
+		padding-bottom: 20px;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 	}
-	
-	.form-buttons{
+
+	.face-panel {
+		width: 800px;
+		height: 500px;
+		padding-top: 20px;
+		padding-bottom: 20px;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.video-style {
+		height: 100%;
+		width: 100%;
+	}
+
+	.form-buttons {
 		display: flex;
 		justify-content: space-around;
 	}
