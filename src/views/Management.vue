9331d9fef48674754565e250fd62ef3bbe68604b
MachineGeek <794763733@qq.com>
2020-10-28 18:28:13 +0800

    modify css of main componet

--- src/views/Management.vue
+++ src/views/Management.vue
@@ -65,20 +65,20 @@
 			<!-- Menu End -->
 		</el-aside>
 		<el-container>
-		    <el-header>
+			<el-header>
 				<div class="main-head">
 					<el-row style="height: 100%;">
 						<el-col :span="1" style="height: 100%;">
 							<div class="flex-center">
-								<el-button icon="el-icon-caret-left" circle class="vertical-center"></el-button>
+								<el-button icon="el-icon-caret-left" circle class="vertical-center" v-on:click="isCollapse = !isCollapse"></el-button>
 							</div>
 						</el-col>
-						<el-col :span="17" style="height: 100%;">
+						<el-col :span="18" style="height: 100%;">
 							<div class="flex-center">
 								<font size="5" color="#1D2F3B" style="align-self:center"><b>{{title}}</b></font>
 							</div>
 						</el-col>
-						<el-col :span="6" style="height: 100%;">
+						<el-col :span="5" style="height: 100%;">
 							<div class="flex-center">
 								<el-input style="width: 250px;" prefix-icon="el-icon-search" v-model="search" placeholder="搜索"></el-input>
 								<el-dropdown>
@@ -97,16 +97,20 @@
 				</div>
 				<el-divider style="margin: 10px 0px;"></el-divider>
 			</el-header>
-		    <el-main>
+			<el-main>
 				<router-view></router-view>
 			</el-main>
-		  </el-container>
+		</el-container>
 	</el-container>
 </template>
 
 <script>
-	import { getCurrent } from '../api/SystemUserApi.js';
-	import { getMenu } from '../api/SystemAuthorityApi.js';
+	import {
+		getCurrent
+	} from '../api/SystemUserApi.js';
+	import {
+		getMenu
+	} from '../api/SystemAuthorityApi.js';
 	export default {
 		name: 'Management',
 		data() {
@@ -125,22 +129,22 @@
 			this.init();
 		},
 		methods: {
-			init(){
+			init() {
 				// 获取菜单
-				getMenu().then((res)=>{
-					if(res.data.success){
+				getMenu().then((res) => {
+					if (res.data.success) {
 						this.menus = res.data.data;
 					}
-				}).catch((err)=>{
+				}).catch((err) => {
 					this.$message.error(err);
 				});
 				// 获取用户信息
-				getCurrent().then((res)=>{
-					if(res.data.success){
+				getCurrent().then((res) => {
+					if (res.data.success) {
 						res.data.data.picture = this.global.apiUrl + res.data.data.picture;
 						this.user = res.data.data;
 					}
-				}).catch((err)=>{
+				}).catch((err) => {
 					this.$message.error(err);
 				});
 			},
@@ -187,8 +191,8 @@
 		width: 100%;
 		height: 100%;
 	}
-	
-	.el-divider--horizontal{
-		margin: 10px 0;
+
+	.el-divider--horizontal {
+		margin: 14px 0;
 	}
 </style>
