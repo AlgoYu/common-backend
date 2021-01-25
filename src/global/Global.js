f75a0c32f3b7f5145dcccf3e050f6f84d8349a6d
MachineGeek <794763733@qq.com>
2021-01-25 18:25:45 +0800

    fix icons.

--- src/global/Global.js
+++ src/global/Global.js
@@ -3,20 +3,18 @@ const apiUrl = "http://127.0.0.1:8888"
 
 // 菜单图标
 const icons = new Map();
-icons.set("SYSTEM","icon-setup_fill");
-icons.set("DATACENTER","icon-chart");
-icons.set("DEVELOP:GENERATOR","icon-barrage_fill");
-icons.set("DEVELOP:DATAMONITOR","icon-database");
-icons.set("DEVELOP:SYSTEMEXCEPTION","icon-flashlight_fill");
+icons.set("SETTING","icon-setup_fill");
+icons.set("DASHBOARD","icon-chart");
+icons.set("GENERATOR","icon-barrage_fill");
+icons.set("MONITOR","icon-database");
+icons.set("EXCEPTION","icon-flashlight_fill");
 icons.set("MANAGEMENT","icon-manage_fill");
-icons.set("DEVELOP","icon-codelibrary-fill");
-icons.set("DEVELOP:DATABASE","icon-database");
-icons.set("MANAGEMENT:SYSTEMUSER","icon-people_fill");
-icons.set("MANAGEMENT:SYSTEMROLE","icon-role");
-icons.set("MANAGEMENT:SYSTEMAUTHORITY","icon-quanxian");
-icons.set("DEVELOP:APPLICATIONMONITOR","icon-stealth_fill");
-icons.set("DEVELOP:DOCUMENTATION","icon-order_fill");
-icons.set("SYSTEM:DICTIONARY","icon-createtask");
+icons.set("DEVTOOLS","icon-codelibrary-fill");
+icons.set("ACCOUNT","icon-people_fill");
+icons.set("ROLE","icon-role");
+icons.set("AUTHORITY","icon-quanxian");
+icons.set("DOCUMENT","icon-order_fill");
+icons.set("SYSTEM","icon-createtask");
 
 // 导出
 export {apiUrl, icons};
